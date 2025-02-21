b20aca9b5308cff1141a5193013f66b31d5cfd88
AnnNat <nannibal@umass.edu>
2025-02-21 14:02:13 -0500

    Just added email as well to the contacts.

--- app/(tabs)/contact.tsx
+++ app/(tabs)/contact.tsx
@@ -1,15 +1,16 @@
 import { View, Text, FlatList, StyleSheet, Image, Platform } from "react-native";
 
 const doctorInfo = [
-  {id: '1', doctorName : 'Dr. Example', doctorPhone : '+1(234)567-8910'},
-  {id: '2', doctorName : 'Dr. Ejemplo', doctorPhone : '+2(345)678-9101'},
-  {id: '3', doctorName : 'Dr. Exemple', doctorPhone : '+3(456)789-1012'}
+  {id: '1', doctorName : 'Dr. Example', doctorPhone : '+1(234)567-8910', doctorEmail : 'example@gmail.com'},
+  {id: '2', doctorName : 'Dr. Ejemplo', doctorPhone : '+2(345)678-9101', doctorEmail : 'ejemplo@gmail.com'},
+  {id: '3', doctorName : 'Dr. Exemple', doctorPhone : '+3(456)789-1012', doctorEmail : 'exemple@gmail.com'}
 ]
 
-const DoctorItem = ({doctorName, doctorPhone}: {doctorName: string, doctorPhone: string}) => (
+const DoctorItem = ({doctorName, doctorPhone, doctorEmail}: {doctorName: string, doctorPhone: string, doctorEmail: string}) => (
   <View style={styles.doctorItem}>
     <Text style={styles.doctorName}>{doctorName}</Text>
     <Text style={styles.doctorContact}>{doctorPhone}</Text>
+    <Text style={styles.doctorEmail}>{doctorEmail}</Text>
   </View>
 );
 
@@ -20,7 +21,7 @@ const ContactYourDoctor = () => {
       <FlatList
         data={doctorInfo}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <DoctorItem doctorName={item.doctorName} doctorPhone={item.doctorPhone} />}
+        renderItem={({ item }) => <DoctorItem doctorName={item.doctorName} doctorPhone={item.doctorPhone} doctorEmail={item.doctorEmail}/>}
       />
     </View>
   );
@@ -55,6 +56,11 @@ const styles = StyleSheet.create({
     color: '#333',
   },
   doctorContact: {
+    fontSize: 16,
+    color: '#666',
+    marginTop: 4,
+  },
+  doctorEmail: {
     fontSize: 16,
     color: '#666',
     marginTop: 4,
